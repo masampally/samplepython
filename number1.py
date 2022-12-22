3a48f2028f00827834de4706b8d80d6ee5724cda
masampally <103827224+masampally@users.noreply.github.com>
2022-12-22 15:45:43 +0530

    Delete number1.py

--- number1.py
+++
@@ -1,8 +0,0 @@
-name = str(input("enter name: "))
-rollnumber = int(input("enter rollnumber: "))
-branch = str(input("enter branch: "))
-print("my name is", name)
-print("my rollnumber is", rollnumber)
-print("my branch", branch)
-
-
